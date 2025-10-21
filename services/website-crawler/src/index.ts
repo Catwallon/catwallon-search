53f6e74e2aa773aae57b0647ea1549c936ddc61a
catwallon <gwechen@breizh.me>
2025-10-21 19:02:02 +0100

    Implement p-limit to launch multiple crawler instances

--- services/website-crawler/src/index.ts
+++ services/website-crawler/src/index.ts
@@ -1,13 +1,18 @@
 import { CrawlerService } from "./service/crawler.service";
 import mongoose from "mongoose";
+import pLimit, { LimitFunction } from "p-limit";
 
 const crawlerService: CrawlerService = new CrawlerService();
 
 try {
   await mongoose.connect("mongodb://mongo:27017");
-  console.log("Connected to MongoDB");
+
+  const limit: LimitFunction = pLimit(8);
+  const tasks = Array(9)
+    .fill(null)
+    .map(() => limit(() => crawlerService.crawl("https://www.wikipedia.org/")));
+
+  await Promise.all(tasks);
 } catch (error) {
   console.error("Connection error:", error);
 }
-
-crawlerService.crawl("https://youtube.com");
